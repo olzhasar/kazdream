4bd19c01a540be279703137e46e4c0dbdb101336
Olzhas <o.arystanov@gmail.com>
2020-08-18 15:24:23 +0600

    Added progress messages

--- load.py
+++ load.py
@@ -16,15 +16,22 @@ def load_data():
 
     tx = g.begin()
 
+    n = 0
+
     for i, row in df.drop_duplicates("group_id").iterrows():
         group = Node("Organization", group_id=row["group_id"], name=row["group"])
         tx.create(group)
+        n += 1
 
     tx.commit()
+
+    print(f"Added {n+1} organizations")
 
     # Creating people and membership records
 
     tx = g.begin()
+
+    n = 0
 
     for i, row in df.drop_duplicates("id").iterrows():
         person = Node(
@@ -40,7 +47,11 @@ def load_data():
         rel = Relationship(person, "Membership", group)
         tx.create(rel)
 
+        n += 1
+
     tx.commit()
+
+    print(f"Added {n+1} people records")
 
 
 if __name__ == "__main__":
